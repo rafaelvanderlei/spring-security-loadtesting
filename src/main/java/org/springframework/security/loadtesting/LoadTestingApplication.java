be960f43c24338f1f21450036934a3ea4e786d8f
Rafael Vanderlei de Souza <rafael.desouza@dataprev.gov.br>
2021-07-17 13:00:09 -0300

    First commit

---
+++ src/main/java/org/springframework/security/loadtesting/LoadTestingApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.security.loadtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/springframework/security/loadtesting/LoadtestingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.security.loadtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadtestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
